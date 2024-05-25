5f496fe32dd03949548d912a40faace9bbe41062
Victor Alves <victoralves28.pg@gmail.com>
2024-05-25 18:06:01 -0300

    feat: generate biased grid

--- server/src/services/n-grid-generator.ts
+++ server/src/services/n-grid-generator.ts
@@ -1,23 +1,56 @@
 import { GridGenerator } from '../domain/services/grid-generator';
 import { RandomLetterGenerator } from '../domain/services/random-letter-generator';
+import { NumberGenerator } from '../domain/services/number-generator';
 
 export class NGridGenerator implements GridGenerator {
     constructor(
         private readonly randomLetterGenerator: RandomLetterGenerator,
+        private readonly numberGenerator: NumberGenerator
     ) { }
 
     makeGrid(size: number, bias: string | null): string[][] {
-        const grid: string[][] = []
+        const grid: string[][] = [];
+        const biased: Set<number> = this.randBias(bias, size);
 
         for (let i = 0; i < size; i++) {
-            grid.push([]);
+            grid.push(new Array(size).fill(null));
 
             for (let j = 0; j < size; j++) {
-                grid[i].push(this.randomLetterGenerator.getLetter())
+                // 1d position based on the 2d coordinates
+                const position = i * size + ((i * size) % size + j);
+
+                if (biased.has(position)) {
+                    grid[i][j] = bias!;
+                } else {
+                    grid[i][j] = this.randomLetterGenerator.getLetter();
+                }
             }
         }
 
-        return grid
+        return grid;
+    }
+
+    private randBias(bias: string | null, size: number): Set<number> {
+        const biased: Set<number> = new Set();
+        if (bias === null) {
+            return biased;
+        }
+
+        // 20% of the grid should include the bias character
+        let counter = Math.ceil(0.2 * size * size);
+
+        while (counter > 0) {
+            const randInt = this.numberGenerator.randomInt(0, size);
+
+            if (biased.has(randInt)) {
+                continue;
+            }
+
+            counter--;
+            biased.add(randInt);
+        }
+
+        return biased;
     }
 
     calcCode(grid: string[][]): string {
--- server/tests/services/n-grid-generator.spec.ts
+++ server/tests/services/n-grid-generator.spec.ts
@@ -3,14 +3,20 @@ import { Mocked } from 'vitest';
 import { mock } from 'vitest-mock-extended';
 import { RandomLetterGenerator } from '../../src/domain/services/random-letter-generator';
 import { NGridGenerator } from '../../src/services/n-grid-generator';
+import { NumberGenerator } from '../../src/domain/services/number-generator';
 
 describe('NGridGenerator', () => {
     let nGridGenerator: NGridGenerator;
     let randomLetterGenerator: Mocked<RandomLetterGenerator>;
+    let numberGenerator: Mocked<NumberGenerator>;
 
     beforeEach(() => {
         randomLetterGenerator = mock<RandomLetterGenerator>();
-        nGridGenerator = new NGridGenerator(randomLetterGenerator);
+        numberGenerator = mock<NumberGenerator>();
+        nGridGenerator = new NGridGenerator(
+            randomLetterGenerator,
+            numberGenerator
+        );
     });
 
     describe('#makeGrid', () => {
@@ -35,6 +41,56 @@ describe('NGridGenerator', () => {
 
             expect(nGridGenerator.makeGrid(N, null)).toEqual(grid);
             expect(randomLetterGenerator.getLetter).toHaveBeenCalledTimes(9);
+        });
+
+        it('should return a biased grid', () => {
+            const N = 5;
+            const bias = 'z';
+
+            const grid = [
+                ['z', 'b', 'c', 'd', 'e'],
+                ['f', 'g', 'z', 'i', 'j'],
+                ['k', 'z', 'm', 'n', 'o'],
+                ['p', 'q', 'r', 's', 'z'],
+                ['z', 'v', 'w', 'x', 'y']
+            ];
+
+            randomLetterGenerator.getLetter
+                .mockReturnValueOnce('b')
+                .mockReturnValueOnce('c')
+                .mockReturnValueOnce('d')
+                .mockReturnValueOnce('e')
+                .mockReturnValueOnce('f')
+                .mockReturnValueOnce('g')
+                .mockReturnValueOnce('i')
+                .mockReturnValueOnce('j')
+                .mockReturnValueOnce('k')
+                .mockReturnValueOnce('m')
+                .mockReturnValueOnce('n')
+                .mockReturnValueOnce('o')
+                .mockReturnValueOnce('p')
+                .mockReturnValueOnce('q')
+                .mockReturnValueOnce('r')
+                .mockReturnValueOnce('s')
+                .mockReturnValueOnce('v')
+                .mockReturnValueOnce('w')
+                .mockReturnValueOnce('x')
+                .mockReturnValueOnce('y');
+
+            numberGenerator.randomInt
+                .mockReturnValueOnce(0)
+                .mockReturnValueOnce(7)
+                .mockReturnValueOnce(7) // repeated
+                .mockReturnValueOnce(11)
+                .mockReturnValueOnce(19)
+                .mockReturnValueOnce(19) // repeated
+                .mockReturnValueOnce(11) // repeated
+                .mockReturnValueOnce(20);
+
+            expect(nGridGenerator.makeGrid(N, bias)).toEqual(grid);
+            expect(randomLetterGenerator.getLetter).toHaveBeenCalledTimes(20);
+            expect(numberGenerator.randomInt).toHaveBeenCalledTimes(8);
+            expect(numberGenerator.randomInt).toHaveBeenCalledWith(0, N);
         });
     });
 });
