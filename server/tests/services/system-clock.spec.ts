21f99af67a59f2536b6bddda5acffa380a8c0242
Victor Alves <victoralves28.pg@gmail.com>
2024-05-25 16:56:36 -0300

    chore: run lint

--- server/tests/services/system-clock.spec.ts
+++ server/tests/services/system-clock.spec.ts
@@ -2,7 +2,7 @@ import { SystemClock } from '../../src/services/system-clock';
 
 describe('SystemClock', () => {
     let systemClock: SystemClock;
-    
+
     beforeEach(() => {
         systemClock = new SystemClock();
         vi.useFakeTimers();
@@ -16,9 +16,12 @@ describe('SystemClock', () => {
         [new Date('2024-05-25T19:30:00.000Z'), '00'],
         [new Date('2024-05-22T17:26:27.000Z'), '27'],
         [new Date('2024-05-30T11:32:59.000Z'), '59'],
-    ])('should return the the correct number of seconds from the current system time', (currentTime, expectedSeconds) => {
-        vi.setSystemTime(currentTime);
+    ])(
+        'should return the the correct number of seconds from the current system time',
+        (currentTime: Date, expectedSeconds: string) => {
+            vi.setSystemTime(currentTime);
 
-        expect(systemClock.getSeconds()).toBe(expectedSeconds);
-    });
+            expect(systemClock.getSeconds()).toBe(expectedSeconds);
+        }
+    );
 });
