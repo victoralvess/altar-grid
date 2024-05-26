0f52a798ac8f59a8d5a8db80c72d1b3fe4ebfea8
Victor Alves <victoralves28.pg@gmail.com>
2024-05-26 16:27:14 -0300

    fix: add minimum height to grid cell

--- client/src/components/GridCell.vue
+++ client/src/components/GridCell.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex items-center justify-center border border-altar-grid p-4 font-bold">{{ char || '' }}</div>
+    <div class="flex items-center justify-center border border-altar-grid p-4 font-bold min-h-[58px]">{{ char || '' }}</div>
 </template>
 
 <script setup lang="ts">
