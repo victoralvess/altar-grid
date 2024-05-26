d4ff96a0f7e84a418a01e0f9dfee038718d7b8a2
Victor Alves <victoralves28.pg@gmail.com>
2024-05-26 15:06:47 -0300

    chore: add body parser to parse user input

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,7 @@ import { env } from 'node:process';
 import express from 'express';
 import type { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 import { ClientException } from './src/domain/exceptions/client-exception';
 import { GRID_SIZE, createGrid, generateCode } from './src/main/init';
 
@@ -29,9 +30,9 @@ app.get('/grid', (req: Request, res: Response) => {
     });
 });
 
-app.post('/code', (req: Request, res: Response) => {
+app.post('/code', bodyParser.json(), (req: Request, res: Response) => {
     res.json({
-        grid: generateCode.generateCode(req.body)
+        grid: generateCode.generateCode(req.body.grid)
     });
 });
 
@@ -39,6 +40,7 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     if (err instanceof ClientException) {
         res.status(err.status).json({ message: err.message });
     } else {
+        console.log(err);
         res.status(500).json({ message: 'Something went terribly wrong' });
     }
 });
