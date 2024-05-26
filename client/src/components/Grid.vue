58e917e32b48bba79b74987f27449598ef47894b
Victor Alves <victoralves28.pg@gmail.com>
2024-05-26 16:26:57 -0300

    feat: add grid component

---
+++ client/src/components/Grid.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div v-for="row in GRID_SIZE" :key="row" class="flex">
+            <GridCell class="flex-1" v-for="char, col in grid[row - 1]" :key="col" :char="char" />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import GridCell from './GridCell.vue';
+
+const GRID_SIZE = 10;
+const grid = ref<string[][]>([]);
+
+for (let i = 0; i < GRID_SIZE; i++) {
+    grid.value.push(new Array(GRID_SIZE).fill(''));
+}
+</script>
+
+<style scoped></style>
