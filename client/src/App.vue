f3ffd314a46a3fc197c9f9da6bb739b1b497e68a
Victor Alves <victoralves28.pg@gmail.com>
2024-05-26 17:49:11 -0300

    feat: handle bias input

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { onUnmounted, ref } from 'vue';
 import Grid from './components/Grid.vue';
 import GridCode from './components/GridCode.vue';
 import axios from 'axios';
@@ -10,10 +10,11 @@ const axiosClient = axios.create({ baseURL: BFF_API_URL });
 const grid = ref<string[][]>([]);
 const code = ref<string | undefined>();
 const live = ref<boolean>(false);
+const bias = ref<string>('');
 
 const fetchGrid = async () => {
   try {
-    const gridResponse = await axiosClient.get('grid', { params: { bias: '' } });
+    const gridResponse = await axiosClient.get('grid', { params: { bias: bias.value } });
     grid.value = gridResponse.data.grid;
   } catch (error) {
     console.error(error);
@@ -41,19 +42,81 @@ const generateGrid = () => {
     await fetchCode()
   }, 2000);
 }
+
+const biasTimeout = ref<boolean>(false);
+const biasError = ref<string | null>(null);
+
+const onBiasInput = () => {
+  if (bias.value === '') {
+    biasError.value = null;
+  }
+}
+
+const biasCounter = ref<number>(0);
+const biasCounterTimeout = ref<number | undefined>();
+
+const onBiasKeyPress = async (event: KeyboardEvent) => {
+  const valid = /^[a-z]$/.test(event.key);
+  
+  if (biasTimeout.value) {
+    biasError.value = 'Timeout error';
+    event.preventDefault();
+    return;
+  }
+
+  if (bias.value.length > 0) {
+    biasError.value = 'Only a single character is allowed';
+    event.preventDefault();
+    return;
+  }
+
+  if (!valid) {
+    biasError.value = 'Character must be a lowercase letter';
+    event.preventDefault();
+    return;
+  }
+
+  biasError.value = null;
+  biasTimeout.value = true;
+
+  await new Promise<void>((resolve) => {
+    clearTimeout(biasCounterTimeout.value);
+    biasCounter.value = 4;
+    biasCounterTimeout.value = setInterval(() => { biasCounter.value--; }, 1000);
+
+    setTimeout(() => {
+      biasTimeout.value = false;
+      biasError.value = null;
+      resolve();
+    }, 4000);
+  });
+}
+
+onUnmounted(() => {
+  clearInterval(gridInterval.value)
+});
 </script>
 
 <template>
   <main class="w-100 max-w-screen-md mx-auto mt-8">
     <div class="flex justify-between items-end">
-      <div class="flex flex-col">
+      <div class="flex flex-col relative">
         <label for="char" class="uppercase text-[#97a0a5] font-bold text-sm mb-2">Character</label>
         <input
           name="char"
           class="border rounded border-altar-input px-2 py-3 w-28"
           type="text"
           placeholder="Character"
+          v-model="bias"
+          @input="onBiasInput"
+          @keypress="onBiasKeyPress"
         >
+        <small
+          v-if="biasError"
+          class="absolute bottom-[-20px] left-0 text-red-500 w-[400px]"
+        >
+          {{ biasTimeout ? `You must wait for another ${biasCounter} seconds` : biasError }}
+        </small>
       </div>
       <div>clock</div>
       <button
